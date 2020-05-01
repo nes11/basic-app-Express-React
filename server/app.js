e65a5904771b8c9ed4ab124b7b0433aadb36112f
Agnes Garoux <agnes.garoux@voxsmart.com>
2020-05-01 11:26:13 +0100

    Adding React client

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const app = express()
-const port = 3005
+const express = require('express');
+const app = express();
+const port = 1330;
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
